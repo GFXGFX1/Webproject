227469c73d26c2237620194370d2bd9fa44c4773
Max2-pacman <yandexmax040@gmail.com>
2025-03-30 19:03:14 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from app import db, login_manager
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    news = db.relationship('News', backref='author', lazy=True)
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
