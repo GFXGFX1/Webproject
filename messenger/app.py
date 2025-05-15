776df72866e2abe2975b7e0e5b3038ecd0326402
Max2-pacman <yandexmax040@gmail.com>
2025-05-15 20:26:14 +0300

    Update app.py

--- messenger/app.py
+++ messenger/app.py
@@ -1,17 +1,72 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-socketio = SocketIO(app, cors_allowed_origins="*")  # Разрешаем кросс-доменные запросы
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+
+
+# Модель пользователя
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+# Создание базы данных
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('register'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['username'] = user.username
+            return redirect(url_for('chat'))
+
+    return render_template('login.html')
+
+
+@app.route('/chat')
+def chat():
+    if 'username' in session:
+        return render_template('chat.html', username=session['username'])
+    return redirect(url_for('login'))
+
 
 @socketio.on('send_message')
-def handle_message(data):
-    emit('receive_message', {'msg': data['msg'], 'user': data['user']}, broadcast=True)
+def handle_send_message(message):
+    socketio.emit('receive_message', message)
+
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)  # Запуск приложения
+    socketio.run(app, allow_unsafe_werkzeug=True)
