22c26bbac6e950bbeef4bd99aed8e345c9c1cab8
GFXGFX1 <topbrawlerdemon@gmail.com>
2025-05-16 13:35:54 +0300

    Update app.py

--- messenger/app.py
+++ messenger/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Flask, render_template, session, redirect, url_for
-from flask_socketio import SocketIO, emit  # Импортируйте emit
+import subprocess
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -11,8 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
-
-# Модель пользователя
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -23,20 +20,13 @@ class Message(db.Model):
     username = db.Column(db.String(50))
     content = db.Column(db.String(500))
 
-# Создайте базу данных и таблицы
+
 with app.app_context():
     db.create_all()
-
-
-# Создание базы данных
-with app.app_context():
-    db.create_all()
-
 
 @app.route('/')
 def index():
     return redirect(url_for('register'))
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -52,7 +42,6 @@ def register():
 
     return render_template('register.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -66,17 +55,16 @@ def login():
 
     return render_template('login.html')
 
-
 @app.route('/chat')
 def chat():
     if 'username' in session:
-        messages = Message.query.all() 
+        messages = Message.query.all()
         return render_template('chat.html', username=session['username'], messages=messages)
     return redirect(url_for('login'))
 
 @socketio.on('send_message')
 def handle_send_message(data):
-    username = session.get('username', 'Аноним')  
+    username = session.get('username', 'Аноним')
     message = data['message']
     new_message = Message(username=username, content=message)
     db.session.add(new_message)
@@ -84,7 +72,12 @@ def handle_send_message(data):
 
     emit('receive_message', {'message': message, 'username': username}, broadcast=True)
 
-
+@socketio.on('open_map')
+def handle_open_map():
+    try:
+        subprocess.Popen(['python', 'map.py'], shell=True)
+    except Exception as e:
+        print(f"Ошибка при открытии карты: {e}")
 
 if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
